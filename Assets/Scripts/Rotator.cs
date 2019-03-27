210da998c019bc63cfad4f53941e0356b71e923f
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2019-03-27 07:34:46 +0100

    ECS Tutorial  - Rotator

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour
+{
+    public float Speed;
+}
+
+public class RotatorSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.deltaTime;
+
+        Entities.ForEach((Rotator r, Transform t) =>
+            t.Rotate(0f, r.Speed * deltaTime, 0f));
+    }
+}
